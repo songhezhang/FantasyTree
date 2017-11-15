41a42c8bfcb946972560e0ca8919a2322f118149
ARCCORE\sozh <sozh@arccore.com>
2017-11-15 13:20:35 +0100

    New Holder

--- src/main/java/com/fantasystep/persistence/TreeManagerSubclassHolder.java
+++ src/main/java/com/fantasystep/persistence/TreeManagerSubclassHolder.java
@@ -1,48 +1,49 @@
 package com.fantasystep.persistence;
 
-import com.fantasystep.systemweaver.item.IntegerSystemSignal;
-import com.fantasystep.systemweaver.item.DataTypeEnum;
-import com.fantasystep.systemweaver.item.Socket;
-import com.fantasystep.domain.Group;
-import com.fantasystep.systemweaver.item.EnumerationDesignSignal;
-import com.fantasystep.systemweaver.item.FloatDesignSignal;
-import com.fantasystep.systemweaver.item.EthernetSwitch;
-import com.fantasystep.systemweaver.item.Composition;
-import com.fantasystep.systemweaver.item.RoutineOperation;
-import com.fantasystep.systemweaver.item.MicroControllerCoreVirtualMachine;
-import com.fantasystep.systemweaver.item.SignalMapping;
-import com.fantasystep.systemweaver.item.ProtocolDataUnit;
-import com.fantasystep.domain.Entity;
-import com.fantasystep.systemweaver.item.DataIdentifier;
-import com.fantasystep.systemweaver.item.IntegerDesignSignal;
-import com.fantasystep.domain.Application;
-import com.fantasystep.domain.Permission;
-import com.fantasystep.systemweaver.item.ByteArrayDesignSignal;
-import com.fantasystep.systemweaver.item.BusConnection;
-import com.fantasystep.systemweaver.item.ByteArraySystemSignal;
-import com.fantasystep.systemweaver.item.BusController;
-import com.fantasystep.systemweaver.item.MicroController;
 import com.fantasystep.systemweaver.item.EthernetNetwork;
-import com.fantasystep.systemweaver.item.EPlatform;
-import com.fantasystep.systemweaver.item.PhysicalLink;
-import com.fantasystep.systemweaver.item.EcuSw;
-import com.fantasystep.systemweaver.item.Frame;
-import com.fantasystep.systemweaver.item.Vlan;
-import com.fantasystep.systemweaver.item.BooleanDesignSignal;
-import com.fantasystep.systemweaver.item.SomeIpSystemSignal;
-import com.fantasystep.systemweaver.item.EnumerationSystemSignal;
-import com.fantasystep.systemweaver.item.LogicComponent;
-import com.fantasystep.domain.User;
-import com.fantasystep.systemweaver.item.ControlRoutine;
+import com.fantasystep.systemweaver.item.ByteArraySystemSignal;
 import com.fantasystep.domain.Organization;
-import com.fantasystep.systemweaver.item.SystemSignalGroup;
-import com.fantasystep.domain.Resource;
-import com.fantasystep.domain.Table;
-import com.fantasystep.domain.EntityGroup;
-import com.fantasystep.systemweaver.item.DesignSignalGroup;
-import com.fantasystep.systemweaver.item.DataInMemory;
+import com.fantasystep.systemweaver.item.SignalMapping;
+import com.fantasystep.systemweaver.item.Socket;
+import com.fantasystep.systemweaver.SysWModel;
+import com.fantasystep.systemweaver.item.BooleanDesignSignal;
 import com.fantasystep.systemweaver.item.FloatSystemSignal;
+import com.fantasystep.systemweaver.item.LogicComponent;
+import com.fantasystep.systemweaver.item.MicroControllerCoreVirtualMachine;
+import com.fantasystep.systemweaver.item.IntegerDesignSignal;
+import com.fantasystep.systemweaver.item.IntegerSystemSignal;
+import com.fantasystep.systemweaver.item.ProtocolDataUnit;
+import com.fantasystep.systemweaver.item.MicroController;
+import com.fantasystep.domain.Table;
+import com.fantasystep.systemweaver.item.DataIdentifier;
+import com.fantasystep.systemweaver.item.SomeIpSystemSignal;
+import com.fantasystep.systemweaver.item.Vlan;
+import com.fantasystep.systemweaver.item.Frame;
+import com.fantasystep.systemweaver.item.EnumerationSystemSignal;
+import com.fantasystep.systemweaver.item.DesignSignalGroup;
+import com.fantasystep.domain.Application;
+import com.fantasystep.systemweaver.item.EnumerationDesignSignal;
+import com.fantasystep.domain.Permission;
+import com.fantasystep.systemweaver.item.RoutineOperation;
+import com.fantasystep.systemweaver.item.ByteArrayDesignSignal;
+import com.fantasystep.domain.Resource;
+import com.fantasystep.systemweaver.item.BusConnection;
 import com.fantasystep.systemweaver.item.DiagnosticEvent;
+import com.fantasystep.systemweaver.item.EcuSw;
+import com.fantasystep.systemweaver.item.SystemSignalGroup;
+import com.fantasystep.systemweaver.item.Composition;
+import com.fantasystep.systemweaver.item.PhysicalLink;
+import com.fantasystep.domain.Entity;
+import com.fantasystep.systemweaver.item.ControlRoutine;
+import com.fantasystep.domain.Group;
+import com.fantasystep.systemweaver.item.BusController;
+import com.fantasystep.systemweaver.item.DataTypeEnum;
+import com.fantasystep.systemweaver.item.EthernetSwitch;
+import com.fantasystep.systemweaver.item.EPlatform;
+import com.fantasystep.domain.User;
+import com.fantasystep.domain.EntityGroup;
+import com.fantasystep.systemweaver.item.DataInMemory;
+import com.fantasystep.systemweaver.item.FloatDesignSignal;
 
 import javax.jws.WebService;
 import javax.xml.bind.annotation.XmlSeeAlso;
@@ -50,7 +51,7 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 import com.fantasystep.persistence.TreeManager;
 
 @WebService
-@XmlSeeAlso({Application.class, SomeIpSystemSignal.class, ProtocolDataUnit.class, EnumerationSystemSignal.class, Group.class, SignalMapping.class, MicroController.class, BooleanDesignSignal.class, ByteArraySystemSignal.class, FloatSystemSignal.class, Table.class, Entity.class, IntegerDesignSignal.class, Composition.class, LogicComponent.class, EntityGroup.class, BusController.class, BusConnection.class, IntegerSystemSignal.class, EnumerationDesignSignal.class, DataTypeEnum.class, DiagnosticEvent.class, EcuSw.class, Permission.class, RoutineOperation.class, PhysicalLink.class, ByteArrayDesignSignal.class, Frame.class, DataInMemory.class, ControlRoutine.class, EPlatform.class, SystemSignalGroup.class, FloatDesignSignal.class, EthernetNetwork.class, MicroControllerCoreVirtualMachine.class, Resource.class, Socket.class, DesignSignalGroup.class, User.class, Vlan.class, DataIdentifier.class, Organization.class, EthernetSwitch.class})
+@XmlSeeAlso({EthernetNetwork.class, ByteArraySystemSignal.class, Organization.class, SignalMapping.class, Socket.class, SysWModel.class, BooleanDesignSignal.class, FloatSystemSignal.class, LogicComponent.class, MicroControllerCoreVirtualMachine.class, IntegerDesignSignal.class, IntegerSystemSignal.class, ProtocolDataUnit.class, MicroController.class, Table.class, DataIdentifier.class, SomeIpSystemSignal.class, Vlan.class, Frame.class, EnumerationSystemSignal.class, DesignSignalGroup.class, Application.class, EnumerationDesignSignal.class, Permission.class, RoutineOperation.class, ByteArrayDesignSignal.class, Resource.class, BusConnection.class, DiagnosticEvent.class, EcuSw.class, SystemSignalGroup.class, Composition.class, PhysicalLink.class, Entity.class, ControlRoutine.class, Group.class, BusController.class, DataTypeEnum.class, EthernetSwitch.class, EPlatform.class, User.class, EntityGroup.class, DataInMemory.class, FloatDesignSignal.class})
 public interface TreeManagerSubclassHolder extends TreeManager {
 
 }
